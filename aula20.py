659d674aa0bf445a55209290980d930d4a94d961
Guilherme Gama <guilhermegamabs@gmail.com>
2023-10-09 14:30:54 -0300

    Exercício len

---
+++ aula20.py
@@ -0,0 +1,11 @@
+"""
+Formatação básica de string
+s - string
+d - int
+f - float
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{1000.423432323:.1f}')
---
+++ aula21.py
@@ -0,0 +1,12 @@
+"""
+Fatiamento de Strings
+ 012345678
+ Olá Mundo
+-987654321
+
+Fatiamento [i:f:p] [::]
+Obs: a função len retorna a qtd de caracteres da str
+"""
+variavel = 'Olá Mundo'
+print(variavel[3:])
+print(variavel[0:9:2])
