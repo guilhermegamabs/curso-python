6c0969a5c57f3158e55b891c95ebe550ef870e18
Guilherme Gama <guilhermegamabs@gmail.com>
2023-10-09 16:18:29 -0300

    Imutáveis

---
+++ aula30.py
@@ -0,0 +1,9 @@
+"""
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'guilherme Gama'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(outra_variavel)
+# print(string.capitalize())
+print(string.zfill(100))
