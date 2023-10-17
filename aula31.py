b8e1d387cac94ee56d436e653d55fa3d74d05d5e
Guilherme Gama <guilhermegamabs@gmail.com>
2023-10-16 22:03:31 -0300

    While

---
+++ aula31.py
@@ -0,0 +1,37 @@
+"""
+While
+"""
+
+# contador = 0
+
+# while contador < 100:
+#     contador += 1
+
+#     if contador == 6:
+#         continue
+
+#     print(contador)
+
+#     if contador == 40:
+#         break
+
+# print('Acabou')
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1
+    linha += 1
+
+print('Acabou')
+
+
+"""
+= += -= *= /= //= **= %=
+"""
