4e5c3a0f8210a9972fb422cfc673cb8144a2f927
Guilherme Gama <guilhermegamabs@gmail.com>
2023-10-23 19:15:28 -0300

    For

---
+++ aula34.py
@@ -0,0 +1,15 @@
+# texto = 'Python'
+
+# i = 0
+# tamanho_string = len(texto)
+
+
+# while i < tamanho_string:
+#     print(texto[i])
+
+#     i +=1
+
+texto = input('Digite algo: ')
+
+for letra in texto:
+    print(letra)
