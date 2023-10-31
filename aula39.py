820dc6e5fdbb5db653f810b020795a79b938bd48
Guilherme Gama <guilhermegamabs@gmail.com>
2023-10-31 19:51:48 -0300

    Tuples

---
+++ aula39.py
@@ -0,0 +1,11 @@
+"""
+Tuples
+"""
+
+nomes = ['Guilherme', 'Maria', 'Luiz']
+nome1, nome2, nome3 = nomes
+
+nomes2 = ['Guilherme', 'Maria', 'Luiz']
+nome4, *_ = nomes2
+
+print(nome1)
---
+++ aula40.py
@@ -0,0 +1,10 @@
+"""enumerate - enumera iteráveis (índices)"""
+
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+lista_enumerada = enumerate(lista)
+print(next(lista_enumerada))
+
+
+print(2**(3**2), (2**3)**2, (2**3)**3)
